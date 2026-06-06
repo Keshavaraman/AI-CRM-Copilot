2396a36ae4147eb92493fd8d14c9296def63f060
keshava <keshavasanjeevi99@gmail.com>
2026-06-06 17:56:20 +0530

    Initial push

---
+++ src/main/java/com/keshava/crmai/CrmaiApplication.java
@@ -0,0 +1,13 @@
+package com.keshava.crmai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmaiApplication.class, args);
+	}
+
+}
